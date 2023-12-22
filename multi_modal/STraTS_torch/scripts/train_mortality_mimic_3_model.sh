ae52df617b574071ebdcad23e94c5c44e8fd36a0
Shaojieee <shaojieee@gmail.com>
2023-12-22 11:07:26 +0800

    Update script

--- multi_modal/STraTS_torch/scripts/train_mortality_mimic_3_model.sh
+++ multi_modal/STraTS_torch/scripts/train_mortality_mimic_3_model.sh
@@ -14,11 +14,11 @@ source activate fyp_multi_modal
 
 cd /home/FYP/szhong005/fyp/multi_modal/STraTS_torch
 
-
+# Original STraTS
 # python -W ignore train.py  \
 #                     --fp16 \
 #                     --train_job "mortality_model" \
-#                     --output_dir "./logs/20231211_1200" \
+#                     --output_dir "./logs/strats" \
 #                     --data_dir "./mortality_mimic_3_benchmark" \
 #                     --d 64 \
 #                     --N 4 \
@@ -32,25 +32,22 @@ cd /home/FYP/szhong005/fyp/multi_modal/STraTS_torch
 #                     --train_batch_size 4 \
 #                     --eval_batch_size 4 \
 #                     --lds 100 \
-#                     --repeats 1 \
+#                     --repeats 5 \
 #                     --num_epochs 100 \
-                
 
+
+# Numerical with new value encoding
 python -W ignore train.py  \
+                    --fp16 \
                     --train_job "mortality_model" \
-                    --output_dir "./logs/strats_with_text_1" \
+                    --output_dir "./logs/strats_new_encoding" \
                     --data_dir "./mortality_mimic_3_benchmark" \
-                    --model_weights "./logs/strats_with_text/best_mortality_model_100_repeat_0.pth" \
-                    --with_text \
+                    -new_value_encoding \
                     --d 64 \
                     --N 4 \
                     --he 4 \
                     --dropout 0.2 \
-                    --text_num_notes 3 \
-                    --period_length 48 \
-                    --text_encoder_model "bioLongformer" \
                     --ts_learning_rate 0.0004 \
-                    --text_learning_rate 0.00002 \
                     --patience 10 \
                     --early_stopper_min_delta 0 \
                     --early_stopper_mode "min" \
@@ -58,8 +55,36 @@ python -W ignore train.py  \
                     --train_batch_size 4 \
                     --eval_batch_size 4 \
                     --lds 100 \
-                    --repeats 1 \
+                    --repeats 5 \
                     --num_epochs 100 \
+                
+
+# STraTS with text
+# python -W ignore train.py  \
+#                     --fp16 \
+#                     --train_job "mortality_model" \
+#                     --output_dir "./logs/strats_with_text_1" \
+#                     --data_dir "./mortality_mimic_3_benchmark" \
+#                     --model_weights "./logs/strats_with_text/best_mortality_model_100_repeat_0.pth" \
+#                     --with_text \
+#                     --d 64 \
+#                     --N 4 \
+#                     --he 4 \
+#                     --dropout 0.2 \
+#                     --text_num_notes 3 \
+#                     --period_length 48 \
+#                     --text_encoder_model "bioLongformer" \
+#                     --ts_learning_rate 0.0004 \
+#                     --text_learning_rate 0.00002 \
+#                     --patience 10 \
+#                     --early_stopper_min_delta 0 \
+#                     --early_stopper_mode "min" \
+#                     --early_stopper_restore_best_weights \
+#                     --train_batch_size 4 \
+#                     --eval_batch_size 4 \
+#                     --lds 100 \
+#                     --repeats 1 \
+#                     --num_epochs 100 \
 
 
             
